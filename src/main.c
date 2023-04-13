e49750b99e47445c00aac2569220ff12b38673af
Tarik Bratic <tarik.bratic@gmx.com>
2023-04-13 12:04:12 +0200

    Create window with player movement

--- src/main.c
+++ src/main.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include "../include/snake.h"
 
-#define WINDOW_HEIGHT 1200
+#define WINDOW_HEIGHT 500
 #define WINDOW_WIDTH 800
 
 typedef struct game {
   SDL_Window *pWindow;
   SDL_Renderer *pRenderer;
+  Snake *pSnk;
 } Game;
 
-int init(Game *pGameStrct);
-void close(Game *pGameStrct);
-void run(Game *pGameStrct);
-void handleInput(Game *pGameStrct, SDL_Event *pEvent);
+int initStructure(Game *pGame);
+void close(Game *pGame);
+void run(Game *pGame);
+void handleInput(Game *pGame, SDL_Event *pEvent);
+void createCanvas(Game *pGame);
+int displayError(Game *pGame);
 
 int main(int argv, char** args) {
   Game g;
 
-  init(&g);
+  initStructure(&g);
   run(&g);
   close(&g);
   
@@ -26,33 +30,85 @@ int main(int argv, char** args) {
 
 }
 
-int init(Game *pGameStrct) {
-  pGameStrct->pWindow = SDL_CreateWindow("Snake.io", SDL_WINDOWPOS_CENTERED, 
+int initStructure(Game *pGame) {
+  // Checks if SDL Library works
+  if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0){
+      printf("Error: %s\n", SDL_GetError());
+      return 0;
+  }
+  // Create window and check for error
+  pGame->pWindow = SDL_CreateWindow("trailClash", SDL_WINDOWPOS_CENTERED, 
     SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+  if (!pGame->pWindow) displayError(pGame);
+
+  // Create renderer and check for error
+  pGame->pRenderer = SDL_CreateRenderer(pGame->pWindow, -1, 
+    SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
+  if (!pGame->pRenderer) displayError(pGame);
+
+  // Creates player and checks for error
+  pGame->pSnk = createSnake(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 
+    pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT);
+  if (!pGame->pSnk) displayError(pGame);
+
 }
 
-void run(Game *pGameStrct) {
+void run(Game *pGame) {
   SDL_Event event;
   int closeRequest = 0;
 
   while(!closeRequest) {
+
     while(SDL_PollEvent(&event)) {
-      if (event.type == SDL_KEYDOWN) closeRequest = 1;
-      else handleInput(pGameStrct, &event);
+      if (event.type == SDL_QUIT) closeRequest = 1;
+      else handleInput(pGame, &event);
     }
+
+    updateRocket(pGame->pSnk);
+    createCanvas(pGame);
+    accelerate(pGame->pSnk);
+
+    SDL_Delay(1000/60-15);
+
   }
 }
 
-void handleInput(Game *pGameStrct, SDL_Event *pEvent) {
-  if (pEvent->type == SDL_KEYDOWN) {
-    switch(pEvent->key.keysym.scancode) {
-      case SDL_SCANCODE_Q:
-        break;
+void handleInput(Game *pGame, SDL_Event *pEvent) {
+  if(pEvent->type == SDL_KEYDOWN){
+        switch(pEvent->key.keysym.scancode){
+            case SDL_SCANCODE_W:
+            case SDL_SCANCODE_UP:
+                accelerate(pGame->pSnk);
+                break;
+            case SDL_SCANCODE_A:
+            case SDL_SCANCODE_LEFT:
+                turnLeft(pGame->pSnk);
+            break;
+            case SDL_SCANCODE_D:
+            case SDL_SCANCODE_RIGHT:
+                turnRight(pGame->pSnk);
+            break;
+        }
     }
-  }
 }
 
-void close(Game *pGameStrct) {
-  if (pGameStrct->pWindow) SDL_DestroyWindow(pGameStrct->pWindow);
+void close(Game *pGame) {
+  if (pGame->pSnk) destroyRocket(pGame->pSnk);
+  if (pGame->pRenderer) SDL_DestroyRenderer(pGame->pRenderer);
+  if (pGame->pWindow) SDL_DestroyWindow(pGame->pWindow);
   SDL_Quit();
+}
+
+void createCanvas(Game *pGame) {
+  SDL_SetRenderDrawColor(pGame->pRenderer,0,0,0,255);
+  SDL_RenderClear(pGame->pRenderer);
+  SDL_SetRenderDrawColor(pGame->pRenderer,230,230,230,255);
+  drawRocket(pGame->pSnk);
+  SDL_RenderPresent(pGame->pRenderer);
+}
+
+int displayError(Game *pGame) {
+  printf("Error: %s\n", SDL_GetError());
+  close(pGame);
+  return 0;  
 }
---
+++ src/snake.c
@@ -0,0 +1,94 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "../include/snake.h"
+
+#define PI 3.14
+
+struct snake {
+  float xCord, yCord;   // Cordinates
+  float xVel, yVel;     // Velocity
+  int angle;
+  int wind_Width, wind_Height;
+  SDL_Renderer *pRenderer;
+  SDL_Texture *pTexture;
+  SDL_Rect snkRect;
+};
+
+Snake *createSnake(int x, int y, SDL_Renderer *pRenderer, int wind_Width, int wind_Height) {
+    Snake *pSnke = malloc(sizeof(struct snake));
+    pSnke->xVel = 0;
+    pSnke->yVel = 0;
+    pSnke->angle = 0;
+    pSnke->wind_Width = wind_Width;
+    pSnke->wind_Height = wind_Height;
+
+    // Load the image
+    SDL_Surface *pSurface = IMG_Load("resources/Ship.png");
+    if (!pSurface) {
+        printf("Error: %s\n", SDL_GetError());
+        return NULL;
+    }
+
+    pSnke->pRenderer = pRenderer;
+    pSnke->pTexture = SDL_CreateTextureFromSurface(pRenderer, pSurface);
+    SDL_FreeSurface(pSurface);
+
+    if (!pSnke->pTexture) {
+        printf("Error: %s\n", SDL_GetError());
+        return NULL;
+    }
+
+    SDL_QueryTexture(pSnke->pTexture, NULL, NULL, 
+      &(pSnke->snkRect.w),&(pSnke->snkRect.h));
+
+    pSnke->snkRect.w /= 4;
+    pSnke->snkRect.h /= 4;
+    pSnke->xCord = x - pSnke->snkRect.w / 2;
+    pSnke->yCord = y - pSnke->snkRect.h / 2;
+
+    return pSnke;
+
+}
+
+void accelerate(Snake *pSnke) {
+    pSnke->xVel = 0.1*sin(pSnke->angle*2*PI/360);
+    pSnke->yVel = 0.1*cos(pSnke->angle*2*PI/360);
+}
+
+void turnLeft(Snake *pSnke) {
+    pSnke->angle-=5;
+}
+
+void turnRight(Snake *pSnke) {
+    pSnke->angle+=5;
+}
+
+void updateRocket(Snake *pSnke) {
+    pSnke->xCord += pSnke->xVel;
+    pSnke->yCord += pSnke->yVel;
+
+    if (pSnke->xCord < 0) {
+        pSnke->xCord += pSnke-> wind_Width;
+    } else if (pSnke->xCord > pSnke->wind_Width) {
+        pSnke->xCord -= pSnke->wind_Width;
+    }
+
+    if (pSnke->yCord < 0) {
+        pSnke->yCord += pSnke->wind_Height;
+    } else if (pSnke->yCord > pSnke->wind_Height) {
+        pSnke->yCord -= pSnke->wind_Height;
+    }
+
+    pSnke->snkRect.x = pSnke->xCord;
+    pSnke->snkRect.y = pSnke->yCord;
+}
+
+void drawRocket(Snake *pSnke) {
+    SDL_RenderCopyEx(pSnke->pRenderer, pSnke->pTexture, NULL,
+        &(pSnke->snkRect), pSnke->angle, NULL, SDL_FLIP_NONE);
+}
+
+void destroyRocket(Snake *pSnke){
+    SDL_DestroyTexture(pSnke->pTexture);
+    free(pSnke);
+}
